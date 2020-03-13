724fa6d49154e3dde37bcaac626866e3ce4dca29
Vinay Kumar Munda <inbox@vinayone.com>
2020-03-13 20:16:20 +0530

    members form update
    members form update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +18,9 @@ import { MembersComponent } from './components/members/members.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/members/members.component.ts
+++ src/app/components/members/members.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-members',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./members.component.scss']
 })
 export class MembersComponent implements OnInit {
+  submitted = false;
+  membersForm: FormGroup;
 
-  constructor() { }
+  
+  constructor() { this.mainForm(); }
 
   ngOnInit(): void {
+  }
+
+  mainForm() {
+    this.membersForm = new FormGroup({
+      userId : new FormControl(''),
+      policyId : new FormControl(''),
+      GroutId : new FormControl(''),
+      ssn : new FormControl(''),
+      lastName : new FormControl(''),
+      firstName : new FormControl(''),
+      inNetworkDeductable : new FormControl(''),
+      outNetworkDeductable : new FormControl(''),
+      difference : new FormControl(''),
+    })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (!this.membersForm.valid) {
+      return false;
+    } else {
+      
+    }
   }
 
 }
